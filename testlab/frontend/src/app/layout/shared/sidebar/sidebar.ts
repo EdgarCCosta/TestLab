82ca61f029bb81a0628456ea65d56b155d84daef
eercilla <eercilla@birt.eus>
2025-12-03 12:34:09 +0100

    Configuración idiomas

--- testlab/frontend/src/app/layout/shared/sidebar/sidebar.ts
+++ testlab/frontend/src/app/layout/shared/sidebar/sidebar.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { importProvidersFrom } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgClass, TranslateModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
