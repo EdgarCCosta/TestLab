60662abe7458666e769a80473ade058375cbb85b
eercilla <eercilla@birt.eus>
2025-12-03 12:42:03 +0100

    Botón idiomas

--- testlab/frontend/src/app/layout/shared/sidebar/sidebar.ts
+++ testlab/frontend/src/app/layout/shared/sidebar/sidebar.ts
@@ -3,14 +3,14 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { importProvidersFrom } from '@angular/core';
-
+import { LanguageSwitcher } from '../language-switcher/language-switcher';
 
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgClass, TranslateModule],
+  imports: [RouterLink, RouterLinkActive, NgClass, TranslateModule, LanguageSwitcher],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
