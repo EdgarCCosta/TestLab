1e3536c1ed09fec48b78d1153bcab21506337624
eercilla <eercilla@birt.eus>
2025-12-15 16:08:25 +0100

    Proyecto con estado dinámico y con componente <listado>

--- testlab/frontend/src/app/features/dashboard/proyecto/proyecto-list/proyecto-list.ts
+++ testlab/frontend/src/app/features/dashboard/proyecto/proyecto-list/proyecto-list.ts
@@ -3,24 +3,53 @@ import { Proyecto } from '../../../../models/proyecto';
 import { ProyectoService } from '../../../../services/proyecto-service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+// import { Modal } from '../../../../layout/shared/modal/modal';
+import { Listado } from '../../../../layout/shared/listado/listado';
+import { signal, effect } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-proyecto-list',
-  imports: [CommonModule],
+  imports: [CommonModule, Listado],
   templateUrl: './proyecto-list.html',
   styleUrl: './proyecto-list.css',
 })
 export class ProyectoList implements OnInit {
 
+ proyectoSelId = signal<string | null>(null);
+
   proyectos: Proyecto[] = [];
 
-  constructor(private _proyectoService: ProyectoService, private router: Router) {}
-
-  ngOnInit(): void {
-    this._proyectoService.getProyectos().subscribe({
-      next: (lista) => this.proyectos = lista
+  constructor(private _proyectoService: ProyectoService, private router: Router) {
+       // efecto: cada vez que cambia proyectoSelId, navega
+    effect(() => {
+      const id = this.proyectoSelId();
+      if (id) {
+        this.detalleProyecto(id);
+      }
     });
+
   }
+
+ngOnInit(): void {
+  this._proyectoService.getProyectos().subscribe({
+    next: (lista) => {
+      this.proyectos = lista.map(p => {
+        // usa la fecha_entrega del modelo Proyecto pero habrá que hablar qué criterios seguimos
+        const vencida = new Date(p.fecha_entrega) > new Date();
+        return {
+          ...p,
+          Proyecto: p.nombre,
+          Descripción: p.descripcion,
+          Estado: vencida
+        };
+      });
+      console.log("Tenemos los proyectos: ", this.proyectos);
+    }
+  });
+}
 
   detalleProyecto(id: string) {
     this.router.navigate(['/proyecto', id]);
--- testlab/frontend/src/app/models/proyecto.ts
+++ testlab/frontend/src/app/models/proyecto.ts
@@ -5,16 +5,19 @@ export interface Proyecto {
   usuario_id: string;
   created_at: string;   // ISO date string
   updated_at: string;   // ISO date string
+  fecha_entrega: string;
 }
 
 export interface CreateProyectoDto {
   nombre: string;
   descripcion: string;
   usuario_id: string;
+  fecha_entrega: string;
 }
 
 export interface UpdateProyectoDto {
   nombre?: string;
   descripcion?: string;
   usuario_id?: string;
+  fecha_entrega?: string;
 }
